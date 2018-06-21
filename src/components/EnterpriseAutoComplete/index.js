149824ba5d162c891acc890b608a6fd6d0e0cc54
IoriCnn <chengeng0502@126.com>
2018-06-21 13:17:35 +0800

    企业控件联想

--- src/components/EnterpriseAutoComplete/index.js
+++ src/components/EnterpriseAutoComplete/index.js
@@ -1,31 +1,46 @@
 
 import React, { Component } from 'react';
 import { AutoComplete } from 'antd';
+import {getEnterprise} from '../../mockdata/Base/commonbase';
+const Option = AutoComplete.Option;
+
+function renderOption(item) {
+    return (
+        <Option key={item.EntCode} text={item.EntName}>
+            {item.EntName}
+        </Option>
+    );
+}
 
 export default class EnterpriseAutoComplete extends Component {
     constructor(props) {
         super(props);
 
+        let enterprise = getEnterprise();
         this.state = {
-            dataSource: [],
+            dataSource: enterprise
         };
     }
 
       handleSearch = (value) => {
-          this.setState({
-              dataSource: !value ? [] : [
-                  value,
-                  value + value,
-                  value + value + value,
-              ],
-          });
+          let enterprise = getEnterprise();
+          if (value) {
+              let enters = enterprise.filter(a => a.EntName.includes(value));
+              this.setState({
+                  dataSource: enters
+              });
+          } else {
+              this.setState({
+                  dataSource: enterprise
+              });
+          }
       }
 
       render() {
           const { dataSource } = this.state;
           return (
               <AutoComplete
-                  dataSource={dataSource}
+                  dataSource={dataSource.map(renderOption)}
                   style={{ width: this.props.width }}
                   onSearch={this.handleSearch}
                   placeholder="输入企业"
