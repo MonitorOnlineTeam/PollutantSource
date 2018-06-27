3476c530cb2db54a045a61b4c7fbcb424a7c22b5
mayantao <123_myt@163.com>
2018-06-27 19:05:04 +0800

    修改格式

--- src/routes/ReplacementPartAdd/index.js
+++ src/routes/ReplacementPartAdd/index.js
@@ -15,7 +15,7 @@ import {
     Row,
     Col
 } from 'antd';
-import styles from './index.less'
+import styles from './index.less';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 /*
 页面：备品备件维护
@@ -39,60 +39,60 @@ export default class ReplacementPartAdd extends Component {
     }
 
     handleFormReset = () => {
-      const { form } = this.props;
-      form.resetFields();
-      this.setState({
-        formValues: {},
-      });
+        const { form } = this.props;
+        form.resetFields();
+        this.setState({
+            formValues: {},
+        });
     };
-    
+
     renderSimpleForm() {
-      const { getFieldDecorator } = this.props.form;
-      const { Option } = Select;
-      return (
-        <Form  layout="inline">
-          <Row gutter={{ md: 6, lg: 24, xl: 48 }}>
-            <Col span={6}  md={6} sm={24}>
-              <FormItem label="名称">
-              {getFieldDecorator(`MaterialName`)(
-                <Input placeholder="请输入名称" />
-            )}
-              </FormItem>
-            </Col>        
-            <Col span={6}    md={6} sm={24}>
-              <FormItem label="品牌">
-                {getFieldDecorator('Brand')(
-                  <Select placeholder="请选择" style={{ width: '100%' }}>
-                    <Option value="0">泽天品牌</Option>
-                    <Option value="1">法兰DN100</Option>
-                    <Option value="1">SDL</Option>
-                  </Select>
-                )}
-              </FormItem>
-            </Col>     
-            <Col span={6}   md={6} sm={24}>
-            <FormItem label="规格型号">
-            {getFieldDecorator(`Specifications`)(
-              <Input placeholder="请输入规格型号" />
-          )}
-            </FormItem>
-          </Col>  
-            <Col span={6}   md={6} sm={24}>
-            <span className={styles.submitButtons}>
-              <Button type="primary" htmlType="submit">
+        const { getFieldDecorator } = this.props.form;
+        const { Option } = Select;
+        return (
+            <Form layout="inline">
+                <Row gutter={{ md: 6, lg: 24, xl: 48 }}>
+                    <Col span={6} md={6} sm={24}>
+                        <FormItem label="名称">
+                            {getFieldDecorator(`MaterialName`)(
+                                <Input placeholder="请输入名称" />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={6} md={6} sm={24}>
+                        <FormItem label="品牌">
+                            {getFieldDecorator('Brand')(
+                                <Select placeholder="请选择" style={{ width: '100%' }}>
+                                    <Option value="0">泽天品牌</Option>
+                                    <Option value="1">法兰DN100</Option>
+                                    <Option value="1">SDL</Option>
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={6} md={6} sm={24}>
+                        <FormItem label="规格型号">
+                            {getFieldDecorator(`Specifications`)(
+                                <Input placeholder="请输入规格型号" />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={6} md={6} sm={24}>
+                        <span className={styles.submitButtons}>
+                            <Button type="primary" htmlType="submit">
                 查询
-              </Button>
-              <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
+                            </Button>
+                            <Button style={{ marginLeft: 8 }} onClick={this.handleFormReset}>
                 重置
-              </Button>           
-            </span>
-          </Col>
-          </Row>
-        </Form>
-      );
+                            </Button>
+                        </span>
+                    </Col>
+                </Row>
+            </Form>
+        );
     }
     renderForm() {
-      return  this.renderSimpleForm();
+        return this.renderSimpleForm();
     }
     render() {
         const { getFieldDecorator } = this.props.form;
@@ -122,61 +122,61 @@ export default class ReplacementPartAdd extends Component {
             width: 80
         }];
 
-        return (    
-          <PageHeaderLayout title="备件备件维护" >    
-          <Card bordered={false} >
-          <div>         
-            <div className={styles.tableListForm}>{this.renderForm()}</div>              
-                <Button style={{marginBottom: 10}} type="primary" onClick={() => {
-                  this.setState({
-                      visible: true,
-                      type: 'list',
-                      title: '入库详细信息查看',
-                      width: 1030
-                  });
-              }}> 查看入库详细 </Button>  
-                <Button style={{marginLeft: 10, marginBottom: 10}} onClick={() => {
-                        this.setState({
-                            visible: true,
-                            type: 'form',
-                            title: '填写入库单',
-                            width: 400
-                        });
-                    }}> 入库 </Button>       
-                <Table
-                    columns={columns}
-                    dataSource={stockdata}
-                    pagination={{
-                      showSizeChanger: true,
-                      showQuickJumper: true,
-                      "total":45,
-                      "pageSize":20,
-                      "current":1
-                    }}
-                    scroll={
-                        {
-                            y: 'calc(100vh - 445px)'
-                        }
-                    }
-                />
-                <Modal
-                    visible={this.state.visible}
-                    title={this.state.title}
-                    width={this.state.width}
-                    onOk={() => {
-                        this.setState({
-                            visible: false
-                        });
-                    }}
-                    onCancel={() => {
-                        this.setState({
-                            visible: false
-                        });
-                    }}>
-                    {
-                        this.state.type === 'form' ? <InStockForm /> : <InStockList />
-                    }
-                </Modal>
-            </div></Card></PageHeaderLayout>);
+        return (
+            <PageHeaderLayout title="备件备件维护" >
+                <Card bordered={false} >
+                    <div>
+                        <div className={styles.tableListForm}>{this.renderForm()}</div>
+                        <Button style={{marginBottom: 10}} type="primary" onClick={() => {
+                            this.setState({
+                                visible: true,
+                                type: 'list',
+                                title: '入库详细信息查看',
+                                width: 1030
+                            });
+                        }}> 查看入库详细 </Button>
+                        <Button style={{marginLeft: 10, marginBottom: 10}} onClick={() => {
+                            this.setState({
+                                visible: true,
+                                type: 'form',
+                                title: '填写入库单',
+                                width: 400
+                            });
+                        }}> 入库 </Button>
+                        <Table
+                            columns={columns}
+                            dataSource={stockdata}
+                            pagination={{
+                                showSizeChanger: true,
+                                showQuickJumper: true,
+                                'total': 45,
+                                'pageSize': 20,
+                                'current': 1
+                            }}
+                            scroll={
+                                {
+                                    y: 'calc(100vh - 445px)'
+                                }
+                            }
+                        />
+                        <Modal
+                            visible={this.state.visible}
+                            title={this.state.title}
+                            width={this.state.width}
+                            onOk={() => {
+                                this.setState({
+                                    visible: false
+                                });
+                            }}
+                            onCancel={() => {
+                                this.setState({
+                                    visible: false
+                                });
+                            }}>
+                            {
+                                this.state.type === 'form' ? <InStockForm /> : <InStockList />
+                            }
+                        </Modal>
+                    </div></Card></PageHeaderLayout>);
     }
 }
