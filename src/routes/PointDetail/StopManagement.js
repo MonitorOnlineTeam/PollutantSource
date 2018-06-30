9f15b04c641568db31ae2b46770d8435cbcb703c
IoriCnn <chengeng0502@126.com>
2018-06-30 22:36:53 +0800

    增加停产管理和点位选择通用控件

--- src/routes/PointDetail/StopManagement.js
+++ src/routes/PointDetail/StopManagement.js
@@ -1,23 +1,19 @@
 // import liraries
 import React, { Component } from 'react';
+import StopManagementModel from '../StopManagement/content';
 
-/* 
+/*
 页面：10、停产管理
 描述：停产情况维护，需要上传附件。描述清楚是勒令停产还是企业减少产能。（自动打标）依据恢复生产动作，可按条件查询停产记录。
 add by cg 18.6.8
-modify by 
+modify by
 */
 class StopManagement extends Component {
-  render() {
-    return (
-      <div
-        style={{ width: '100%',
-      height: 'calc(100vh - 225px)' }}
-      >
-        模块5
-      </div>
-    );
-  }
+    render() {
+        return (
+            <StopManagementModel />
+        );
+    }
 }
 // make this component available to the app
 export default StopManagement;
