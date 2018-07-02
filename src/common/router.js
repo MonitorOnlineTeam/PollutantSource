c890699b88d7edf2108484ac9fb155ba48b362d0
ahuihui6688723 <37164099+ahuihui6688723@users.noreply.github.com>
2018-07-02 09:47:48 +0800

    d

--- src/common/router.js
+++ src/common/router.js
@@ -93,6 +93,10 @@ export const getRouterData = (app) => {
             component: dynamicWrapper(app, ['points'], () =>
           import('../routes/PointDetail')),
         },
+        '/monitor/emergency/emergencydetailinfo/:exceptionhandleid': {
+            component: dynamicWrapper(app, ['points'], () =>
+          import('../routes/EmergencyTodoList/EmergencyDetailInfo')),
+        },
         '/monitor/pointdetail/:pointcode/processflowdiagram': {
             component: dynamicWrapper(app, ['points'], () =>
           import('../routes/PointDetail/ProcessFlowDiagram')),
