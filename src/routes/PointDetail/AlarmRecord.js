c71e694bc4a41e2a492bd75b12382b472e92ac5a
dongxiaoyun123 <1227906469@qq.com>
2018-07-06 13:16:28 +0800

    报警和预警

--- src/routes/PointDetail/AlarmRecord.js
+++ src/routes/PointDetail/AlarmRecord.js
@@ -2,106 +2,162 @@
 import React, { Component } from 'react';
 import RangePicker_ from '../../components/PointDetail/RangePicker_';
 import AlarmTypeSelect from '../../components/AlarmTypeSelect/index';
+import {routerRedux} from 'dva/router';
 import { Card,
     InputNumber,
     Row,
     Col,
-    Modal
+    Modal,
+    Table,
+    Button
 } from 'antd';
+import {connect} from 'dva';
 import PollutantSelect_ from '../../components/PointDetail/PollutantSelect_';
-import { Button } from 'antd';
-import { Table, Icon, Divider } from 'antd';
+import moment from 'moment';
 import ExceptionProcessing from '../../mockdata/Base/Code/T_Cod_ExceptionProcessing';
 import Process from '../../components/AlarmRecord/Process';
+
+@connect(() => ({
+
+}))
+
 class AlarmRecord extends Component {
     constructor(props) {
         super(props);
         this.state = {
             rangeDate: [],
+            selectedRowKeys: [],
 
         };
     }
-
     _handleDateChange=(date, dateString) => {
         console.log(date);// [moment,moment]
         console.log(dateString);// ['2018-06-23','2018-06-25']
         // this.state.rangeDate = date;
         this.setState({rangeDate: date});
     };
+    onSelectChange = (selectedRowKeys) => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys);
+        this.setState({ selectedRowKeys });
+    }
+    attentionClick=(e) => {
+        this.setState({attentionvisible: true});
+    }
+    stationclick = () => {
+        this.props.dispatch(routerRedux.push(`/monitor/pointdetail/${this.props.match.params.pointcode}/DataQuery`));
+    }
     render() {
+        const { selectedRowKeys } = this.state;
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange,
+            hideDefaultSelections: true,
+            selections: [{
+                key: 'all-data',
+                text: 'Select All Data',
+                onSelect: () => {
+                    this.setState({
+                        selectedRowKeys: [...Array(46).keys()], // 0...45
+                    });
+                },
+            }, {
+                key: 'odd',
+                text: 'Select Odd Row',
+                onSelect: (changableRowKeys) => {
+                    let newSelectedRowKeys = [];
+                    newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                        if (index % 2 !== 0) {
+                            return false;
+                        }
+                        return true;
+                    });
+                    this.setState({ selectedRowKeys: newSelectedRowKeys });
+                },
+            }, {
+                key: 'even',
+                text: 'Select Even Row',
+                onSelect: (changableRowKeys) => {
+                    let newSelectedRowKeys = [];
+                    newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                        if (index % 2 !== 0) {
+                            return true;
+                        }
+                        return false;
+                    });
+                    this.setState({ selectedRowKeys: newSelectedRowKeys });
+                },
+            }],
+            onSelection: this.onSelection,
+        };
         const columns = [{
             title: '污染物',
             dataIndex: 'PollutantName',
-            key: '1',
+            key: 'PollutantName',
             width: 110,
 
         },
         { title: '报警时间',
             dataIndex: 'AlarmTime',
-            key: '2',
-            width: 180,
+            key: 'AlarmTime',
+            width: 150,
 
         }, {
             title: '报警类别',
             dataIndex: 'AlarmType',
-            key: '3',
+            key: 'AlarmType',
             width: 110,
 
         }, {
             title: '报警状态',
             dataIndex: 'State',
-            key: '4',
+            key: 'State',
             width: 110,
 
         },
         {
             title: '报警持续时长(小时)',
             dataIndex: 'AlarmContinuedTime',
-            key: '5',
-            width: 180,
+            key: 'AlarmContinuedTime',
+            width: 150,
+
+        },
+        {
+            title: '运维单',
+            dataIndex: 'ViewOperation',
+            key: 'ViewOperation',
+            width: 80,
+            render: (text, record, index) => { return <Button type="primary" shape="circle" icon="link" size={'small'} onClick={this.stationclick} id={record.key} />; }
 
         },
         {
             title: '预计恢复时间',
             dataIndex: 'ExceptRecoverTime',
-            key: '6',
+            key: 'ExceptRecoverTime',
             width: 180,
 
         }, {
             title: '描述',
             dataIndex: 'AlarmMsg',
-            key: '7',
-            width: 1250,
+            key: 'AlarmMsg',
+            width: 800,
 
         }];
-        this._AlarmTypeChange = (value, selectedOptions) => {
-            console.log(value);
-            console.log(selectedOptions);
-
-            // ExceptionProcessing.map(item => {
-            //     if(item[]
-            // });
-        };
-
         const data = ExceptionProcessing;
         return (
             <div style={{ width: '100%', height: 'calc(100vh - 225px)' }}>
-                <Card title="北京绿都供暖-锅炉小号烟囱">
-                    <Row gutter={16} style={{marginBottom: 40, marginLeft: 5}}>
+                <Card>
+                    <Row gutter={16} style={{marginBottom: 40, marginLeft: 5, marginTop: 20}}>
                         <Col className="gutter-row" span={4}>
                             <span className="gutter-box">报警类别: <AlarmTypeSelect onChange={this._AlarmTypeChange} width={150} /></span>
                         </Col>
                         <Col className="gutter-row" span={4}>
                             <span className="gutter-box">污染物: <PollutantSelect_ onChange={this._handlePollutantChange} style={{width: 150}} /></span>
                         </Col>
-                        <Col className="gutter-row" span={5}>
-                            <span className="gutter-box">时间: <RangePicker_ dateFormat="YYYY-MM-DD" dateValue={this.state.rangeDate} onChange={this._handleDateChange} /></span>
+                        <Col className="gutter-row" span={6}>
+                            <span className="gutter-box">时间: <RangePicker_ style={{width: 350}} format="YYYY-MM-DD HH:mm:ss" onChange={this._handleDateChange} dateValue={this.state.rangeDate} /></span>
                         </Col>
                         <Col className="gutter-row" span={4}>
-                            <span className="gutter-box">报警持续时长: <InputNumber min={1} max={1000} style={{width: 60}} defaultValue={3} />小时以上</span>
-                        </Col>
-                        <Col className="gutter-row" span={2}>
-                            <span className="gutter-box"><Button style={{width: 90}} type="primary">查看运维</Button></span>
+                            <span className="gutter-box">报警持续时长: <InputNumber min={1} max={1000} style={{width: 60}} />小时以上</span>
                         </Col>
                         <Col className="gutter-row" span={2}>
                             <span className="gutter-box"><Button style={{width: 90}} type="primary" onClick={() => {
@@ -132,13 +188,14 @@ class AlarmRecord extends Component {
                             this.state.type === 'add' ? <Process /> : null
                         }
                     </Modal>
-                    <Row gutter={16}>
+                    <Row gutter={18} style={{marginTop: 30}}>
                         <Col className="gutter-row" span={24}>
-                            <div className="gutter-box"><Table
+                            <Table
                                 columns={columns}
                                 dataSource={data}
-                                scroll={{ x: 1950, y: 600 }}
-                            /></div>
+                                scroll={{ x: 1950, y: 'calc(100vh - 465px)' }}
+                                rowSelection={rowSelection}
+                            />
                         </Col>
                     </Row>
                 </Card>
