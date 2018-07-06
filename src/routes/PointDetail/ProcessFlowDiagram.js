d8719854ea50b95b3f2cd783c6a6a3c875160df5
ahuihui6688723 <37164099+ahuihui6688723@users.noreply.github.com>
2018-07-06 16:06:16 +0800

    a

--- src/routes/PointDetail/ProcessFlowDiagram.js
+++ src/routes/PointDetail/ProcessFlowDiagram.js
@@ -6,7 +6,14 @@ export default class ProcessFlowDiagram extends Component {
     render() {
         const { match} = this.props;
         const pointcode = match.params.pointcode; // 任务ID
-        const status = '0'; // 任务
+        let status = '0'; // 任务
+        const alarmType=["bjldgn01","dtgjhh11102","dtgrjx110"];
+        if(alarmType.indexOf(pointcode)>-1){
+            status="1";
+        }
+        else{
+            status="0";
+        }
         return (
             <GyPic DGIMN={pointcode} status={status}/>
         );
