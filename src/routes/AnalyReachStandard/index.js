155979e0bf71592fd8ec9970f4ba2f5efb955d6c
mayantao <123_myt@163.com>
2018-07-16 16:10:50 +0800

    增加底部说明

--- src/routes/AnalyReachStandard/index.js
+++ src/routes/AnalyReachStandard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Select, DatePicker} from 'antd';
+import {Select, DatePicker, Card} from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import ReactEcharts from 'echarts-for-react';
 import moment from 'moment';
@@ -59,7 +59,7 @@ export default class AnalyReachStandard extends Component {
             yAxis: {
                 type: 'category',
                 data: ['大唐滨州发电有限公司一厂', '大唐滨州发电有限公司二厂',
-                    '大唐国际红河发电公司', '大唐甘肃发电有限公司', '辽宁大唐国际葫芦岛热电厂', '大唐供热集团']
+                    '大唐国际红河发电公司', '大唐甘肃发电有限公司', '辽宁大唐国际葫芦岛热电厂', '大唐供热公司']
             },
             series: [
                 {
@@ -77,12 +77,17 @@ export default class AnalyReachStandard extends Component {
         return (
             <PageHeaderLayout title="企业排污达标率">
                 <RangePicker defaultValue={[moment('2018/07/01', dateFormat), moment('2018/08/01', dateFormat)]} format={dateFormat} />
-                <Select defaultValue="Dust" style={{ width: 120, marginLeft: 40 }}>
-                    <Option value="Dust">颗粒物</Option>
+                <Select defaultValue="SO2" style={{ width: 120, marginLeft: 40 }}>                 
                     <Option value="SO2">SO2</Option>
                     <Option value="NOx">NOx</Option>
+                    <Option value="Dust">烟尘</Option>
                 </Select>
-                <ReactEcharts option={option} lazyUpdate={true} notMerge={true} id="rightLine" style={{ width: '100%', height: 'calc(100vh - 380px)' }} />
+                <ReactEcharts option={option} lazyUpdate={true} notMerge={true} id="rightLine" style={{ width: '100%', height: 'calc(100vh - 430px)' }} />
+                <Card title="说明" style={{width: '100%'}} >
+                    <p>达标率计算方法：达标率=[1+(执行标准-达标量）÷执行标准]×100%</p>
+                    <p>2018/07/01~2018/08/01,大唐滨州发电有限公司一厂达标率最高为99%，大唐供热公司达标率最低为65%</p>
+                    <p>大唐甘肃发电有限公司达标率同比增长较快，2017/07/01~2017/08/01 达标率为70%，今年同期达标率为81%</p>
+                </Card>
             </PageHeaderLayout>
         );
     }
