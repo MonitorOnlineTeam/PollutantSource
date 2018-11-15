ec1d8cdbf2c8e1587be578e4a5afd4db68124656
ahuihui6688723 <37164099+ahuihui6688723@users.noreply.github.com>
2018-11-15 19:34:51 +0800

    1

---
+++ src/utils/enum.js
@@ -0,0 +1,14 @@
+
+// 接口调用的返回状态
+export const RequstResult = {
+    AuthenticationFailure: "-1", // 身份验证失败
+    Fail: "0", // 失败
+    Success: "1", // 成功
+    Exception: "2" // 发生异常
+};
+
+// 运维任务类型
+export const PatrolTaskType = {
+    PatrolTask: 1, // 巡检任务
+    ExceptionTask: 2 // 故障任务
+};
