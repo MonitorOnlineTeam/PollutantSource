1c04dec56386c319265447676372062303d937ab
dongxiaoyun123 <1227906469@qq.com>
2018-11-28 13:18:25 +0800

    视频3版

--- src/components/PointDetail/HistoryVideo.js
+++ src/components/PointDetail/HistoryVideo.js
@@ -4,75 +4,131 @@ import img from '../../../public/video.png';
 import styles from './Video.less';
 import VideoAlarmData from '../../mockdata/PointDetail/VideoAlarmData.json';
 import HistoryData from '../../mockdata/PointDetail/HisotryMonitorData.json';
-
+import { connect } from 'dva';
+import moment from 'moment';
 const {RangePicker} = DatePicker;
+@connect(({loading, videolist}) => ({
+    ...loading,
+    historyVideolist: videolist.historyVideolist,
+    AlarmInfoList: videolist.AlarmInfoList,
+}))
 export default class HistoryVideo extends Component {
+    constructor(props) {
+        super(props);
+        var defaultBeginTime = moment().add('days', -1).format('YYYY-MM-DD 00:00:00');
+        var date = moment().format('YYYY-MM-DD 23:59:59');
+        this.state = {
+            beginDate: defaultBeginTime,
+            endDate: date,
+            DGIMN: 'sgjt001003',
+            MonitorTime: '2018-11-27 08:00:00'
+        };
+    }
+    componentWillMount() {
+        this.onChange();
+    };
+    onChange = () => {
+        this.props.dispatch({
+            type: 'videolist/gethistoryVideoList',
+            payload: {
+                DGIMN: this.state.DGIMN,
+                MonitorTime: this.state.MonitorTime,
+            },
+        });
+        this.props.dispatch({
+            type: 'videolist/getAlarmHistory',
+            payload: {
+                DGIMN: this.state.DGIMN,
+                beginDate: this.state.beginDate,
+                endDate: this.state.endDate,
+            },
+        });
+    }
+    rangepickerOnChange=(date, dateString) => {
+        this.setState({beginDate: dateString[0], endDate: dateString[1]});
+        this.props.dispatch({
+            type: 'videolist/updateAlarmHistory',
+            payload: {
+                DGIMN: this.state.DGIMN,
+                beginDate: date[0].format('YYYY-MM-DD 00:00:00'),
+                endDate: date[1].format('YYYY-MM-DD 23:59:59'),
+            },
+        });
+    }
     render() {
+        console.log(this.state.beginDate);
+        console.log(this.state.endDate);
+        const dateFormat = 'YYYY/MM/DD';
+        const monthFormat = 'YYYY/MM';
         const alarmColumns = [{
             title: '因子',
             dataIndex: 'PollutantCode',
-            width: 50,
+
         }, {
             title: '报警时间段',
-            dataIndex: 'AlarmTime',
-            width: 130,
+            dataIndex: 'RangeTime',
         }];
 
-        const historyColumns = [{
-            title: '监测时间',
-            dataIndex: 'MonitorTime',
-            width: 50,
-        }, {
-            title: '实测烟尘(mg/m³)',
-            dataIndex: 'Col1',
-            width: 50,
-        }, {
-            title: 'SO2(mg/m³)',
-            dataIndex: 'Col2',
-            width: 50,
-            render: (text, record) => {
-                if (text > 8) {
-                    return (<span style={{color: 'red'}}>{text}</span>);
-                } else {
-                    return text;
-                }
-            }
-        }, {
-            title: 'NOX(mg/m³)',
-            dataIndex: 'Col3',
-            width: 50,
-            render: (text, record) => {
-                if (text > 14) {
-                    return (<span style={{color: 'red'}}>{text}</span>);
-                } else {
-                    return text;
-                }
-            }
-        }, {
-            title: '流量(m³/h)',
-            dataIndex: 'Col4',
-            width: 50,
-        }, {
-            title: '氧含量(%)',
-            dataIndex: 'Col5',
-            width: 50,
-        }, {
-            title: '流速(L/S)',
-            dataIndex: 'Col6',
-            width: 50,
-        }, {
-            title: '温度(℃)',
-            dataIndex: 'Col7',
-            width: 50,
-        }, {
-            title: '湿度(%)',
-            dataIndex: 'Col8',
-            width: 50,
-        }, {
-            title: '烟气静压(Pa)',
-            dataIndex: 'Col9',
-            width: 50,
-        }];
+        const historyColumns = [
+            {
+                title: '监测时间',
+                dataIndex: 'MonitorTime',
+
+            }, {
+                title: '实测烟尘(mg/m³)',
+                dataIndex: '01',
+
+            },
+            {
+                title: '实测二氧化硫(mg/m³)',
+                dataIndex: '02',
+
+            },
+            {
+                title: '实测氮氧化物(mg/m³)',
+                dataIndex: '03',
+
+            },
+            {
+                title: '烟尘(mg/m³)',
+                dataIndex: 'zs01',
+
+            },
+            {
+                title: '二氧化硫(mg/m³)',
+                dataIndex: 'zs02',
+
+            },
+            {
+                title: '氮氧化物(mg/m³)',
+                dataIndex: 'zs03',
+
+            },
+            {
+                title: '流量(m³/h)',
+                dataIndex: 'b02',
+
+            }, {
+                title: '氧含量(%)',
+                dataIndex: 's01',
+
+            }, {
+                title: '流速(L/S)',
+                dataIndex: 's02',
+
+            }, {
+                title: '温度(℃)',
+                dataIndex: 's03',
+
+            }, {
+                title: '湿度(%)',
+                dataIndex: 's05',
+
+            }, {
+                title: '烟气静压(Pa)',
+                dataIndex: 's08',
+
+            }];
 
         return (
             <div>
@@ -80,11 +136,11 @@ export default class HistoryVideo extends Component {
                     <Col xl={17} lg={24} md={24} sm={24} xs={24} style={{ marginBottom: 24, height: 490 }}>
                         <div className={styles.videoComponent} />
                     </Col>
-                    <Col xl={6} lg={24} md={24} sm={24} xs={24}>
+                    <Col xl={7} lg={24} md={24} sm={24} xs={24}>
 
                         <Card className={styles.hisYunStyle}>
                             <Row>
-                                <Col span={24} ><RangePicker /></Col>
+                                <Col span={24} >   <RangePicker defaultValue={[moment(this.state.beginDate, dateFormat), moment(this.state.endDate, dateFormat)]} onChange={this.rangepickerOnChange} /></Col>
                             </Row>
                             <Divider type="horizontal" />
                             <Row>
@@ -107,7 +163,7 @@ export default class HistoryVideo extends Component {
                             报警历史记录
                             <Row>
                                 <Col className={styles.gutterleft} span={24} >
-                                    <Table size="large" bordered={true} columns={alarmColumns} dataSource={VideoAlarmData} pagination={false} scroll={{y: 120, x: 100}} />
+                                    <Table size="large" columns={alarmColumns} dataSource={this.props.AlarmInfoList} pagination={false} scroll={{y: 120, x: 100}} />
                                 </Col>
                             </Row>
                         </Card>
@@ -116,20 +172,21 @@ export default class HistoryVideo extends Component {
                 </Row>
                 <Table
                     columns={historyColumns}
-                    dataSource={HistoryData}
+                    dataSource={this.props.historyVideolist}
+                    pagination={false}
                     bordered={true}
-                    pagination={{
-                        showSizeChanger: true,
-                        showQuickJumper: true,
-                        'total': 45,
-                        'pageSize': 20,
-                        'current': 1
-                    }}
-                    scroll={
-                        {
-                            y: 500
-                        }
-                    }
+                    // pagination={{
+                    //     showSizeChanger: true,
+                    //     showQuickJumper: true,
+                    //     'total': 45,
+                    //     'pageSize': 20,
+                    //     'current': 1
+                    // }}
+                    // scroll={
+                    //     {
+                    //         y: 500
+                    //     }
+                    // }
                 />
             </div>
         );
--- src/routes/PointDetail/RealVideo.js
+++ src/routes/PointDetail/RealVideo.js
@@ -1,17 +1,122 @@
 import React, { Component } from 'react';
-import RealTimeVideo from '../../components/PointDetail/RealTimeVideo';
-import {Row, Col, Button, List, Table, Card, Select, Divider} from 'antd';
+// import Answersheet from '../../components/Video/webVideoCtrl';
+import {Row, Col, Button, List, Table, Card, Select, Divider, Message} from 'antd';
 import MonitorData from '../../mockdata/PointDetail/VideoMonitorData.json';
 import StateData from '../../mockdata/PointDetail/StateData.json';
 import styles from './video.less';
+import { connect } from 'dva';
+import moment from 'moment';
 const Option = Select.Option;
+@connect(({loading, videolist}) => ({
+    ...loading,
+    list: videolist.list,
+    total: videolist.total,
+    pageSize: videolist.pageSize,
+    pageIndex: videolist.pageIndex,
+    requstresult: videolist.requstresult,
+}))
 /*
 页面：4、实时视频
 描述：可以和数据、参数、报警等联动查看
 add by myt 18.7.9
 */
+
 export default class RealVideo extends Component {
+    componentWillMount() {
+        this.onChange();
+        // this.OnloadVideo();
+        // this.clickLogin();
+    };
+    onChange = () => {
+        this.props.dispatch({
+            type: 'videolist/fetchuserlist',
+            payload: {
+                DGIMN: 'sgjt001003',
+            },
+        });
+    }
+
+    //      OnloadVideo=() => {
+    //          debugger
+    //          // 全局保存当前选中窗口
+    //          var g_iWndIndex = 0; // 可以不用设置这个变量，有窗口参数的接口中，不用传值，开发包会默认使用当前选择窗口
+    //          var message = 'failed';
+    //          var messagess = 'failed';
+    //          var vwidth; // 视频画面宽度
+    //          var vheight; // 视频画面高度
+    //          // 检查插件是否已经安装过
+    //          if (Answersheet.I_CheckPluginInstall() === -1) {
+    //              Message.error('您还未安装过插件，请在IE浏览器下，点击确定下载安装！');
+    //              window.location.href = 'D:/lepingshipin/WebComponents.exe';
+    //              return;
+    //          }
+    //          // 初始化插件参数及插入插件
+    //          Answersheet.I_InitPlugin(vwidth, vheight, {
+    //              iWndowType: 1,
+    //              cbSelWnd: function(xmlDoc) {
+    //                  g_iWndIndex = 0;
+    //              }
+    //          });
+    //          Answersheet.I_InsertOBJECTPlugin('divPlugin');
+
+    //          // 检查插件是否最新
+    //          if (Answersheet.I_CheckPluginVersion() === -1) {
+    //              Message.error('检测到新的插件版本，点击确定下载升级！');
+    //              window.location.href = 'D:/lepingshipin/WebComponents.exe';
+    //              return;
+    //          }
+
+    //          // 窗口事件绑定
+    //          window.bind({
+    //              resize: function() {
+    //                  var $Restart = document.getElementById('restartDiv');
+    //                  if ($Restart.length > 0) {
+    //                      var clientWidth = document.body.clientWidth;
+    //                      var clientHeight = document.body.clientHeight;
+    //                      $Restart.css({
+    //                          width: clientWidth.width + 'px',
+    //                          height: clientHeight.height + 'px'
+    //                      });
+    //                  }
+    //              }
+    //          });
+    //      }
+    //      // 登录
+    //  clickLogin=() => {
+    //      ;
+    //      var message = 'failed';
+    //      var szIP = '223.84.240.165';
+    //      var szPort = '1001';
+    //      var szUsername = 'admin';
+    //      var szPassword = 'admin123';
+
+    //      if (szIP === '' || szPort === '') {
+    //          message = "{ 'flag': 'false', 'message': 'IP或端口不能为空！' }";
+    //          return Message.error(message);
+    //      }
+
+    //      var iRet = Answersheet.I_Login(szIP, 1, szPort, szUsername, szPassword, {
+    //          success: function(xmlDoc) {
+    //              message = "{ 'flag': true, 'message':' 登录成功！监控视频连接中...' }";
+    //          },
+    //          error: function() {
+    //              message = "{ 'flag': false, 'message':' 登录失败！' }";
+    //          }
+    //      });
+
+    //      if (iRet === -1) {
+    //          message = "{ 'flag': true, 'message':' 已登录过！视频连接中...' }";
+    //      }
+
+    //      if (iRet === undefined) {
+    //          message = "{ 'flag': true, 'message':' 登录中...' }";
+    //      }
+
+    //      return message;
+    //  }
+
     render() {
+        console.log(this.props.list);
         const columns = [{
             title: '参数',
             dataIndex: 'Para',
@@ -21,22 +126,19 @@ export default class RealVideo extends Component {
             dataIndex: 'Val',
             width: 50,
         }];
-
+        const data = this.props.list;
         return (
-            <div >
+            <div>
                 <Row gutter={24}>
                     <Col span={17} style={{ marginBottom: 10, height: 480 }}>
-                        <div className={styles.videoComponent} />
+                        <div className={styles.videoComponent} id="restartDiv" />
                     </Col>
                     <Col span={7} >
                         <Card>
                             <Row>
                                 <Col span={16}>
                                     <Select defaultValue="摄像头1号" style={{ width: 150 }}>
-                                        <Option value="jack">摄像头1号</Option>
-                                        <Option value="lucy">摄像头2号</Option>
-                                        <Option value="disabled">摄像头3号</Option>
-                                        <Option value="Yiminghe">摄像头4号</Option>
+                                        {data.map(item => <Option key={item.VedioDevice_ID}>{item.VedioCamera_Name}</Option>)}
                                     </Select>
                                 </Col>
                                 <Col span={8} ><Button icon="picture">抓图</Button></Col>
@@ -96,7 +198,6 @@ export default class RealVideo extends Component {
                         </Card>
                     </Col>
                 </Row>
-
                 <Row gutter={24}>
                     <Col xl={12} lg={24} sm={24} xs={24}>
                         <Card title="实时数据">
