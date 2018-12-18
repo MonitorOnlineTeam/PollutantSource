e67d0afaf6907460bfd06ad23535ceaa8a044f61
IoriCnn <chengeng0502@126.com>
2018-12-18 11:20:58 +0800

    增加实时视频

--- src/models/videolist.js
+++ src/models/videolist.js
@@ -1,12 +1,9 @@
-import {
-    Model
-} from '../dvapack';
-import {
-    getList, deleteVideoInfo, gethistoryVideoList, updateVideoInfos, addVideoInfo, getAlarmHistory, updateAlarmHistory, userDgimnDataFilter
-} from '../services/videodata';
+import { Model } from '../dvapack';
+import { getList, deleteVideoInfo, gethistoryVideoList, updateVideoInfos, addVideoInfo, getAlarmHistory, updateAlarmHistory, userDgimnDataFilter } from '../services/videodata';
+import { queryprocesschart } from '../services/api';
+
 export default Model.extend({
     namespace: 'videolist',
-
     state: {
         editUser: null,
         requstresult: null,
@@ -19,250 +16,73 @@ export default Model.extend({
         pageSize: 10,
         pageIndex: 1,
         reason: null,
-    },
-    subscriptions: {
-        setup({
-            dispatch,
-            history
-        }) {
-            history.listen((location) => {
-                // if (location.pathname === '/monitor/sysmanage/userinfo') {
-                //     // 初始化testId的值为10
-                //     dispatch({
-                //         type: 'fetchuserlist',
-                //         payload: {
-                //         },
-                //     });
-                // }
-            });
-        },
+        realdata: []
     },
     effects: {
-        * fetchuserlist({
-            payload: {
-                // pageIndex,
-                // pageSize,
-                DGIMN
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            const result = yield call(getList, {DGIMN: DGIMN});
-
+        * fetchuserlist({payload}, { call, put, update, select }) {
+            const result = yield call(getList, {DGIMN: payload.DGIMN});
             if (result.requstresult === '1') {
                 yield update({
                     requstresult: result.requstresult,
-                    list: result.data,
-                    // total: result.total,
-                    // pageIndex: pageIndex,
-                    // pageSize: pageSize
+                    list: result.data
                 });
             } else {
                 yield update({
                     requstresult: result.requstresult,
-                    list: [],
-                    // total: 0,
-                    // pageIndex: null,
-                    // pageSize: null
+                    list: []
                 });
             }
         },
-        * updateVideoInfos({payload: {
-            VedioDevice_Name,
-            VedioDevice_No,
-            VedioDevice_Position,
-            IP,
-            User_Name,
-            User_Pwd,
-            Device_Port,
-            VedioCamera_Name,
-            VedioCamera_No,
-            VedioCamera_Position,
-            ProduceDate,
-            VedioCamera_Version,
-            Longitude,
-            Latitude,
-            VedioDevice_ID,
-            CameraMonitorID,
-            VedioCamera_ID,
-            DGIMN
-        }}, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            ;
-            const result = yield call(updateVideoInfos, {
-                VedioDevice_Name: VedioDevice_Name,
-                VedioDevice_No: VedioDevice_No,
-                VedioDevice_Position: VedioDevice_Position,
-                IP: IP,
-                User_Name: User_Name,
-                User_Pwd: User_Pwd,
-                Device_Port: Device_Port,
-                VedioCamera_Name: VedioCamera_Name,
-                VedioCamera_No: VedioCamera_No,
-                VedioCamera_Position: VedioCamera_Position,
-                ProduceDate: ProduceDate,
-                VedioCamera_Version: VedioCamera_Version,
-                Longitude: Longitude,
-                Latitude: Latitude,
-                VedioDevice_ID: VedioDevice_ID,
-                CameraMonitorID: CameraMonitorID,
-                VedioCamera_ID: VedioCamera_ID,
-            });
+        * updateVideoInfos({payload}, { call, put, update, select }) {
+            const result = yield call(updateVideoInfos, {...payload});
             yield update({
                 requstresult: result.requstresult,
                 reason: result.reason
             });
             yield put({
                 type: 'fetchuserlist',
-                payload: {DGIMN},
+                payload: payload.DGIMN,
             });
         },
-        * addVideoInfos({
-            payload: {
-                VedioDevice_Name,
-                VedioDevice_No,
-                VedioDevice_Position,
-                IP,
-                User_Name,
-                User_Pwd,
-                Device_Port,
-                VedioCamera_Name,
-                VedioCamera_No,
-                VedioCamera_Position,
-                ProduceDate,
-                VedioCamera_Version,
-                Longitude,
-                Latitude,
-                DGIMN,
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            ;
-            const result = yield call(addVideoInfo, {
-                VedioDevice_Name: VedioDevice_Name,
-                VedioDevice_No: VedioDevice_No,
-                VedioDevice_Position: VedioDevice_Position,
-                IP: IP,
-                User_Name: User_Name,
-                User_Pwd: User_Pwd,
-                Device_Port: Device_Port,
-                VedioCamera_Name: VedioCamera_Name,
-                VedioCamera_No: VedioCamera_No,
-                VedioCamera_Position: VedioCamera_Position,
-                ProduceDate: ProduceDate,
-                VedioCamera_Version: VedioCamera_Version,
-                Longitude: Longitude,
-                Latitude: Latitude,
-                DGIMN: DGIMN,
-            });
+        * addVideoInfos({payload}, { call, put, update, select }) {
+            const result = yield call(addVideoInfo, {...payload});
             yield update({
                 requstresult: result.requstresult,
                 reason: result.reason
             });
             yield put({
                 type: 'fetchuserlist',
-                payload: {DGIMN},
+                payload: payload.DGIMN,
             });
         },
-        * deleteVideoInfo({
-            payload: {
-                DGIMN,
-                VedioCamera_ID,
-                VedioDevice_ID,
-                CameraMonitorID
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            ;
-            const result = yield call(deleteVideoInfo, {
-                VedioCamera_ID: VedioCamera_ID,
-                VedioDevice_ID: VedioDevice_ID,
-                CameraMonitorID: CameraMonitorID
-            });
+        * deleteVideoInfo({ payload }, { call, put, update, select }) {
+            const result = yield call(deleteVideoInfo, {...payload});
             yield update({
                 requstresult: result.requstresult,
                 editUser: result.data[0]
             });
             yield put({
                 type: 'fetchuserlist',
-                payload: {
-                    DGIMN
-                },
+                payload: payload.DGIMN,
             });
         },
-        * gethistoryVideoList({
-            payload: {
-                DGIMN,
-                MonitorTime
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            ;
-            const result = yield call(gethistoryVideoList, {
-                DGIMN: DGIMN,
-                MonitorTime: MonitorTime,
-            });
+        * gethistoryVideoList({payload}, { call, put, update, select }) {
+            const result = yield call(gethistoryVideoList, {...payload});
             if (result.requstresult === '1') {
                 yield update({
                     requstresult: result.requstresult,
-                    historyVideolist: result.data,
-                    // total: result.total,
-                    // pageIndex: pageIndex,
-                    // pageSize: pageSize
+                    historyVideolist: result.data
                 });
             } else {
                 yield update({
                     requstresult: result.requstresult,
-                    historyVideolist: [],
-                    // total: 0,
-                    // pageIndex: null,
-                    // pageSize: null
+                    historyVideolist: []
                 });
             }
-            // yield put({
-            //     type: 'fetchuserlist',
-            //     payload: {
-            //         DGIMN
-            //     },
-            // });
         },
         * getAlarmHistory({
-            payload: {
-                DGIMN,
-                beginDate,
-                endDate
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
-            ;
-            const result = yield call(getAlarmHistory, {
-                DGIMN: DGIMN,
-                beginDate: beginDate,
-                endDate: endDate,
-            });
+            payload}, { call, put, update, select }) {
+            const result = yield call(getAlarmHistory, {...payload});
             if (result.requstresult === '1') {
                 yield update({
                     requstresult: result.requstresult,
@@ -275,22 +95,9 @@ export default Model.extend({
                 });
             }
         },
-        * updateAlarmHistory({
-            payload: {
-                DGIMN,
-                beginDate,
-                endDate
-            }
-        }, {
-            call,
-            put,
-            update,
-            select
-        }) {
+        * updateAlarmHistory({payload}, { call, put, update, select }) {
             const result = yield call(updateAlarmHistory, {
-                DGIMN: DGIMN,
-                beginDate: beginDate,
-                endDate: endDate,
+                ...payload
             });
             if (result.requstresult === '1') {
                 yield update({
@@ -305,13 +112,14 @@ export default Model.extend({
             }
             yield put({
                 type: 'getAlarmHistory',
-                payload: {
-                    DGIMN,
-                    beginDate,
-                    endDate
-                },
+                payload: {...payload},
             });
         },
+        * queryprocesschart({payload}, {call, update}) {
+            debugger;
+            const res = yield call(queryprocesschart, {...payload});
+            yield update({ realdata: res });
+        }
     },
     reducers: {
         save(state, action) {
@@ -330,7 +138,6 @@ export default Model.extend({
             return {
                 ...state,
                 ...action.payload
-                // currentUser: action.payload,
             };
         },
         changeNotifyCount(state, action) {
@@ -349,12 +156,10 @@ export default Model.extend({
             };
         },
         setCurrentMenu(state, action) {
-            // ;
             return {
                 ...state,
                 currentMenu: action.payload,
             };
         },
-
     },
 });
