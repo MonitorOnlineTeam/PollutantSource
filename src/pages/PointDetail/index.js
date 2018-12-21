ed176ed6c75077a47d2b0d9dad582c9c0f8340a4
wjwgit <w417287508@163.com>
2018-12-21 15:52:19 +0800

    update

--- src/pages/PointDetail/index.js
+++ src/pages/PointDetail/index.js
@@ -1,17 +1,20 @@
 // import liraries
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Breadcrumb, Tabs, Icon, Select, Button } from 'antd';
+import { Modal,Breadcrumb, Tabs, Icon, Select, Button,Card,Avatar,Row,Col,Badge,Tag,Input,Form } from 'antd';
 import { Link, Switch, Redirect } from 'dva/router';
 import { getRoutes } from '../../utils/utils';
 import styles from './index.less';
 import Cookie from 'js-cookie';
 const { TabPane } = Tabs;
 const Option = Select.Option;
+const Search = Input.Search;
+const { Meta } = Card;
 import router from 'umi/router';
 
 @connect(({points, loading}) => ({
     pointInfo: points.selectpoint,
+    loadingModel: loading.effects['overview/querydatalist'],
     isloading: loading.effects['points/querysinglepointinfo']
 }))
 class PointDetail extends Component {
@@ -32,7 +35,9 @@ class PointDetail extends Component {
             recordType: [
                 'RepairHistoryRecods',
                 'JzHistoryRecords'
-            ]
+            ],
+            modalVisible:false,
+            loadingCard:true
         };
     }
     componentDidMount() {
@@ -43,7 +48,34 @@ class PointDetail extends Component {
             }
         });
     }
+    openModal = (params) => {
+        debugger;
+        this.setState({
+            modalVisible: true,
+            loadingCard:false
+            // pointName: params.PointName
+        });
+        // this.updateState({
+        //     queryDGIMNs: params.DGIMNs,
+        //     // queryDate: this.props.clickDate,
+        //     pointDaysTableData: []
 
+        // });
+        // this.getPointDaysTableData(1);
+    }
+    handleModalOk = (e) => {
+        console.log(e);
+        this.setState({
+            modalVisible: false,
+        });
+    }
+
+    handleModalCancel = (e) => {
+    // console.log(e);
+        this.setState({
+            modalVisible: false,
+        });
+    }
     render() {
         const { match, routerData, location,children } = this.props;
         Cookie.set('seldgimn', match.params.pointcode);
@@ -52,7 +84,6 @@ class PointDetail extends Component {
         if (location.pathname.indexOf('qcontrollist') === -1) {
             activeKey = location.pathname.replace(`${match.url}/`, '');
         }
-
         return (
             <div
                 style={{ width: '100%',
@@ -74,7 +105,7 @@ class PointDetail extends Component {
                     <div className={styles.pageHeader} style={{}}>
                         <img src='../../../point.png'  style={{width:37,marginTop:-1}}/>
                         <span style={{color:'#ccc',marginLeft:10}}>当前排口：</span>
-                        <span style={{cursor:'pointer'}}>{pointInfo.pointName} <Icon type="down" style={{marginLeft:10,width:15}} /></span>
+                        <span style={{cursor:'pointer'}} onClick={this.openModal}>{pointInfo.pointName} <Icon type="down" style={{marginLeft:10,width:15}} /></span>
                         {/* <Select defaultValue={pointInfo.pointName} style={{ width: 200 }}>
                             <Option value={pointInfo.pointcode}>{pointInfo.pointName}</Option>
                         </Select> */}
@@ -95,6 +126,89 @@ class PointDetail extends Component {
                         {children}
                     </div>
                 </div> : ''}
+                <Modal
+                    visible={this.state.modalVisible}
+                    title={'当前排口: '+pointInfo.pointName}
+                    onOk={this.handleModalOk}
+                    onCancel={this.handleModalCancel}
+                    width='1200px'
+                    footer={[]}
+                    >
+                    <Form layout="inline" style={{marginBottom: 10}}>
+                            <Row gutter={8}>
+                                <Col span={3} >
+                                    <Search
+                                        placeholder="请输入排口关键字"
+                                        onSearch={(value) => {
+                                            this.setState({
+                                                UserAccount: value
+                                            });
+                                            this.props.dispatch({
+                                                type: 'userinfo/fetchuserlist',
+                                                payload: {
+                                                    pageIndex: 1,
+                                                    pageSize: 10,
+                                                    UserAccount: value,
+                                                },
+                                            });
+                                        }}
+                                        style={{ width: 200 }}
+                                    /></Col>
+                            </Row>
+                        </Form>
+                    <div style={{ background: '#ECECEC', padding: '30px',minHeight:'400px' }} className={styles.pointModal}>
+                        <Row gutter={48}>
+                            <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                                <Card  bordered={false} loading={this.state.loadingCard}>
+                                    <div className={styles.cardContent}>
+                                        <p><Badge style={{ backgroundColor: 'rgb(255,198,0)' }} dot={true}/><span className={styles.pointName}>焦炉小号烟囱7</span></p>
+                                        <p className={styles.TEF}>传输有效率<span>61%</span></p>
+                                        <p className={styles.TEF}>类型：<span>废弃</span></p>
+                                    </div>
+                                    <div style={{position:"absolute",top:5,right:-25}}>
+                                        <Tag color="#2db7f5">故障</Tag>
+                                    </div>
+                                </Card>
+                            </Col>
+                            <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                                <Card  bordered={false}>
+                                    <div className={styles.cardContent}>
+                                        <p><Badge dot={true}/><span className={styles.pointName}>焦炉小号烟囱7</span></p>
+                                        <p className={styles.TEF}>传输有效率<span>61%</span></p>
+                                        <p className={styles.TEF}>类型：<span>废弃</span></p>
+                                    </div>
+                                    <div style={{position:"absolute",top:5,right:-25}}>
+                                        <Tag color="#2db7f5">故障</Tag>
+                                    </div>
+                                </Card>
+                            </Col>
+                            <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                                <Card  bordered={false}>
+                                    <div className={styles.cardContent}>
+                                        <p><Badge dot={true}/><span className={styles.pointName}>焦炉小号烟囱7</span></p>
+                                        <p className={styles.TEF}>传输有效率<span>61%</span></p>
+                                        <p className={styles.TEF}>类型：<span>废弃</span></p>
+                                    </div>
+                                    <div style={{position:"absolute",top:5,right:-25}}>
+                                        <Tag color="#2db7f5">故障</Tag>
+                                    </div>
+                                </Card>
+                            </Col>
+                            <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+                                <Card  bordered={false}>
+                                    <div className={styles.cardContent}>
+                                        <p><Badge dot={true}/><span className={styles.pointName}>焦炉小号烟囱7</span></p>
+                                        <p className={styles.TEF}>传输有效率<span>61%</span></p>
+                                        <p className={styles.TEF}>类型：<span>废弃</span></p>
+                                    </div>
+                                    <div style={{position:"absolute",top:5,right:-25}}>
+                                        <Tag color="#2db7f5">故障</Tag>
+                                    </div>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </div>
+                </Modal>
             </div>
         );
     }
