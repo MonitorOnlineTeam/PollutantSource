20a7a126aad20202027f5d63a94026386beca69e
薛朋宇 <15210415451@163.com>
2018-12-27 08:53:28 +0800

    update

--- src/pages/PointInfo/AddAnalyzerSys.js
+++ src/pages/PointInfo/AddAnalyzerSys.js
@@ -16,27 +16,21 @@ import {
     Modal,
     Divider,
 } from 'antd';
-import PageHeader from '../../components/PageHeader';
 import {
     connect
 } from 'dva';
-import {
-    routerRedux
-} from 'dva/router';
-import ModalMap from '../PointInfo/ModalMap';
-import { isNullOrUndefined } from 'util';
 const FormItem = Form.Item;
 const Option = Select.Option;
-const Search = Input.Search;
 @connect(({
     loading,
-    pointinfo
+     pointinfo
 }) => ({
     ...loading,
     reason: pointinfo.reason,
-    requstresult: pointinfo.requstresult,
-    editpoint: pointinfo.editpoint,
-    userlist: pointinfo.userlist,
+    addalyzersys_requstresult: pointinfo.addalyzersys_requstresult,
+    getanalyzersysmnmodel_requstresult: pointinfo.getanalyzersysmnmodel_requstresult,
+    editAnalyzerSys: pointinfo.editAnalyzerSys,
+    editalyzersys_requstresult: pointinfo.editalyzersys_requstresult
 }))
 @Form.create()
 export default class AddAnalyzerSys extends Component {
@@ -44,142 +38,87 @@ export default class AddAnalyzerSys extends Component {
         super(props);
         this.state = {
             DGIMN: null,
+            ID: null,
+            Disabled:false,
         };
     }
     componentWillMount() {
-        const DGIMN = this.props.DGIMN;
-        if (DGIMN !== 'null') {
+        this.setState({
+            DGIMN:this.props.DGIMN,
+        })
+        const ID = this.props.ID;
+        console.log(ID);
+        if (ID !=='null') {
             this.setState({
-                DGIMN: DGIMN,
-                DGIMNdisabled: true,
+                ID: ID,
+                Disabled:true,
             });
             this.props.dispatch({
-                type: 'pointinfo/getpoint',
+                type: 'pointinfo/getanalyzersysmnmodel',
                 payload: {
-                    DGIMN: DGIMN,
+                    ID: ID,
                     callback: () => {
-                        if (this.props.requstresult === '1') {
-                            console.log(this.props.editpoint);
-                            console.log(this.props.editpoint);
-                            this.setState({
-                                OutputType: this.props.editpoint.OutputTypeId === '1',
-                                IsSj: this.props.editpoint.IsSj === '1',
-                                address: this.props.editpoint.Address,
-                                coordinate: this.props.editpoint.longitude + ',' + this.props.editpoint.latitude
-                            }, () => {
-                                this.props.form.setFieldsValue({
-                                    PointName: this.props.editpoint.pointName,
-                                    DGIMN: this.props.editpoint.DGIMN,
-                                    OutputType: this.state.OutputType,
-                                    MobilePhone: this.props.editpoint.mobilePhone,
-                                    Linkman: this.props.editpoint.linkman === isNullOrUndefined ? '' : this.props.editpoint.linkman,
-                                    PointType: this.props.editpoint.PointTypeId === '' ? undefined : this.props.editpoint.PointTypeId,
-                                    PollutantType: this.props.editpoint.pollutantType === '' ? undefined : this.props.editpoint.pollutantType,
-                                    IsSj: this.state.IsSj,
-                                    OutputDiameter: this.props.editpoint.OutputDiameter,
-                                    OutputHigh: this.props.editpoint.OutputHigh,
-                                    OutPutWhitherCode: this.props.editpoint.OutPutWhitherCode === '' ? undefined : this.props.editpoint.OutPutWhitherCode,
-                                    Sort: this.props.editpoint.Sort === isNullOrUndefined ? 1 : this.props.editpoint.Sort,
-                                    OperationerId: this.props.editpoint.OperationerId === '' ? undefined : this.props.editpoint.OperationerId,
-                                    Address: this.state.address,
-                                    Coordinate: this.state.coordinate
-                                });
+                        if (this.props.getanalyzersysmnmodel_requstresult === '1') {
+                            console.log(this.props.editAnalyzerSys);
+                            this.props.form.setFieldsValue({
+                                Type: this.props.editAnalyzerSys.Type,
+                                Manufacturer: this.props.editAnalyzerSys.Manufacturer,
+                                ManufacturerCode: this.props.editAnalyzerSys.ManufacturerCode
                             });
                         }
                     }
                 },
             });
         }
-
-        this.getOperationer();
     }
-     onRef1 = (ref) => {
-         this.child = ref;
-     }
     
     handleSubmit = (e) => {
         e.preventDefault();
         let flag = true;
         this.props.form.validateFieldsAndScroll((err, values) => {
             const that = this;
-            if (this.state.DGIMN === null) {
+            if (that.state.ID === null) {
                 if (!err && flag === true) {
                     that.props.dispatch({
-                        type: 'pointinfo/addpoint',
+                        type: 'pointinfo/addalyzersys',
                         payload: {
-                            DGIMN: values.DGIMN,
-                            PointName: values.PointName,
-                            PointType: values.PointType === undefined ? '' : values.PointType,
-                            PollutantType: values.PollutantType === undefined ? '' : values.PollutantType,
-                            IsSj: values.IsSj === true ? '1' : '0',
-                            Coordinate: values.Coordinate === undefined ? '' : values.Coordinate,
-                            OutPutWhitherCode: values.OutPutWhitherCode === undefined ? '' : values.OutPutWhitherCode,
-                            Sort: values.Sort === undefined ? '' : values.Sort,
-                            Linkman: values.Linkman === undefined ? '' : values.Linkman,
-                            OutputDiameter: values.OutputDiameter === undefined ? '' : values.OutputDiameter,
-                            OutputHigh: values.OutputHigh === undefined ? '' : values.OutputHigh,
-                            OutputType: values.OutputType === true ? '1' : '0',
-                            Address: values.Address,
-                            MobilePhone: values.MobilePhone === undefined ? '' : values.MobilePhone,
-                            OperationerId: values.OperationerId,
+                            DGIMN: this.state.DGIMN,
+                            Manufacturer: values.Manufacturer,
+                            Type: values.Type === undefined ? '' : values.Type,
+                            ManufacturerCode: values.ManufacturerCode,
                             callback: () => {
-                                if (this.props.requstresult === '1') {
-                                    this.success();
+                                if (this.props.addalyzersys_requstresult === '1') {
+                                    message.success('添加成功！').then(() => this.props.ChildVisitable());
                                 } else {
                                     message.error(this.props.reason);
                                 }
                             }
                         },
                     });
-                } else {
-                }
+                } 
             } else {
                 if (!err && flag === true) {
                     that.props.dispatch({
-                        type: 'pointinfo/editpoint',
-                        payload: {
-                            DGIMN: values.DGIMN,
-                            PointName: values.PointName,
-                            PointType: values.PointType === undefined ? '' : values.PointType,
-                            PollutantType: values.PollutantType === undefined ? '' : values.PollutantType,
-                            IsSj: values.IsSj === true ? '1' : '0',
-                            Coordinate: values.Coordinate === undefined ? '' : values.Coordinate,
-                            OutPutWhitherCode: values.OutPutWhitherCode === undefined ? '' : values.OutPutWhitherCode,
-                            Sort: values.Sort === undefined ? '' : values.Sort,
-                            Linkman: values.Linkman === undefined ? '' : values.Linkman,
-                            OutputDiameter: values.OutputDiameter === undefined ? '' : values.OutputDiameter,
-                            OutputHigh: values.OutputHigh === undefined ? '' : values.OutputHigh,
-                            OutputType: values.OutputType === true ? '1' : '0',
-                            Address: values.Address,
-                            MobilePhone: values.MobilePhone === undefined ? '' : values.MobilePhone,
-                            OperationerId: values.OperationerId,
-                            callback: () => {
-                                if (this.props.requstresult === '1') {
-                                    this.success();
-                                } else {
-                                    message.error(this.props.reason);
-                                }
-                            }
-                        },
+                      type: 'pointinfo/editalyzersys',
+                      payload: {
+                        ID:this.state.ID,
+                        Manufacturer: values.Manufacturer,
+                        ManufacturerCode: values.ManufacturerCode,
+                        callback: () => {
+                          if (this.props.editalyzersys_requstresult === '1') {
+                            message.success('编辑成功！').then(() => this.props.ChildVisitable());
+                          } else {
+                            message.error(this.props.reason);
+                          }
+                        }
+                      },
                     });
-                } else {
-
                 }
             }
         });
     };
-     success = () => {
-         let index = this.props.dispatch(routerRedux.push(`/sysmanage/PointInfo`));
-         if (this.state.DGIMN !== null) {
-             message.success('修改成功', 3).then(() => index);
-         } else {
-             message.success('新增成功', 3).then(() => index);
-         }
-     };
      render() {
          const {getFieldDecorator} = this.props.form;
-         // const UserId = this.props.match.params.UserId;
-
          return (
              <div>
               <Form onSubmit={this.handleSubmit} >
@@ -188,14 +127,19 @@ export default class AddAnalyzerSys extends Component {
                          <Col span={24} >
                              <FormItem labelCol={{span: 8}}
                                  wrapperCol={{span: 12}}
-                                 label="排口类型" > {
-                                     getFieldDecorator('PointType',
+                                 label="设备类型" > {
+                                     getFieldDecorator('Type',
                                          {
                                              initialValue: undefined,
+                                              rules: [{
+                                                required: true,
+                                                message: '请选择设备类型!'
+                                              }]
                                          }
-                                     )(<Select placeholder="请选择" >
-                                         <Option value="1" > 工艺废气排放口 </Option>
-                                         <Option value="2" > 燃烧废气排放口 </Option>
+                                     )(<Select placeholder="请选择"  disabled={this.state.Disabled} >
+                                         <Option value="1" > 气态污染物CEMS设备仪器 </Option>
+                                         <Option value="2" > 烟尘污染物CEMS设备仪器 </Option>
+                                         <Option value="3" > 颗粒污染物CEMS设备仪器 </Option>
                                      </Select>)
                                  } </FormItem>
                          </Col>
@@ -204,14 +148,14 @@ export default class AddAnalyzerSys extends Component {
                          <Col span={24} >
                              <FormItem labelCol={{span: 8}}
                                  wrapperCol={{span: 12}}
-                                 label="排口名称" > {
-                                     getFieldDecorator('PointName', {
+                                 label="供应商名称" > {
+                                     getFieldDecorator('Manufacturer', {
                                          rules: [{
                                              required: true,
-                                             message: '请输入排口名称!'
+                                             message: '请输入供应商名称!'
                                          } ]
 
-                                     })(<Input placeholder="排口名称" />)
+                                     })(<Input placeholder="供应商名称" />)
                                  } </FormItem>
                          </Col>
                      </Row>
@@ -219,13 +163,13 @@ export default class AddAnalyzerSys extends Component {
                       <Col span={24} >
                              <FormItem labelCol={{span: 8}}
                                  wrapperCol={{span: 12}}
-                                 label="排口编号" > {
-                                     getFieldDecorator('DGIMN', {
+                                 label="供应商编号" > {
+                                     getFieldDecorator('ManufacturerCode', {
                                          rules: [{
                                              required: true,
-                                             message: '请输入排口编号!'
+                                             message: '请输入供应商编号!'
                                          }]
-                                     })(<Input placeholder="排口编号"
+                                     })( < Input placeholder = "供应商编号"
                                          disabled={
                                              this.state.DGIMNdisabled
                                          }
@@ -239,13 +183,6 @@ export default class AddAnalyzerSys extends Component {
                              <Button type="primary"
                                  htmlType="submit">
                           保存
-                             </Button>
-                             <Divider type="vertical" />
-                             <Button type="dashed"
-                                 onClick={
-                                     () => this.props.dispatch(routerRedux.push(`/sysmanage/PointInfo`))
-                                 } >
-                          返回
                              </Button>
                          </Col>
                      </Row>
