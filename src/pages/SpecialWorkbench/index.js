d5c8b1414551034b5921e872e432ec3d81117ae6
wjwgit <w417287508@163.com>
2018-12-29 17:34:31 +0800

    update

--- src/pages/SpecialWorkbench/index.js
+++ src/pages/SpecialWorkbench/index.js
@@ -805,7 +805,7 @@ class SpecialWorkbench extends Component {
             <span style={{marginRight:20}}>排放口:<span style={{marginLeft:5,color:'rgb(72,145,255)'}}>{model.PointTotal}</span></span>
             <span style={{marginRight:20}}>运行:<span style={{marginLeft:5,color:'rgb(93,192,94)'}}>{model.RuningNum}</span></span>
             <span style={{marginRight:20}}>离线:<span style={{marginLeft:5,color:'rgb(244,5,4)'}}>{model.OffLine}</span></span>
-            <span style={{marginRight:20}}>异常:<span style={{marginLeft:5,color:'orange'}}>{model.ExceptionNum}</span></span>
+            <span style={{marginRight:20}}>异常:<span style={{marginLeft:5,color:'gold'}}>{model.ExceptionNum}</span></span>
             <span style={{marginRight:20}}>关停:<span style={{marginLeft:5,color:'rgb(208,145,14)'}}>{model.StopNum}</span></span>
                </span>;
     }
