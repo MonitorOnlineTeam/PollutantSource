a6740ec7190acde3d1c5c5ede0babdd61b4940a1
薛朋宇 <15210415451@163.com>
2019-01-07 08:44:25 +0800

    update

--- src/services/pointinfo.js
+++ src/services/pointinfo.js
@@ -23,6 +23,7 @@ export async function addpoint(params) {
         PointType: params.PointType,
         PollutantType: params.PollutantType,
         IsSj: params.IsSj,
+        RunState: params.RunState,
         Coordinate: params.Coordinate,
         OutPutWhitherCode: params.OutPutWhitherCode,
         Linkman: params.Linkman,
@@ -68,6 +69,7 @@ export async function editpoint(params) {
         PointType: params.PointType,
         PollutantType: params.PollutantType,
         IsSj: params.IsSj,
+        RunState: params.RunState,
         Coordinate: params.Coordinate,
         OutPutWhitherCode: params.OutPutWhitherCode,
         Linkman: params.Linkman,
@@ -97,71 +99,71 @@ export async function deletepoint(params) {
 }
 // 获取排口分析仪
 export async function getanalyzersys(params) {
-  const body = {
-    DGIMN: params.DGIMN
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerSysMN', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        DGIMN: params.DGIMN
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerSysMN', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 添加分析仪
 export async function addalyzersys(params) {
-  const body = {
-    DGIMN: params.DGIMN,
-    Manufacturer: params.Manufacturer,
-    ManufacturerCode: params.ManufacturerCode,
-    Type: params.Type,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/AddAnalyzerSysMN', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        DGIMN: params.DGIMN,
+        Manufacturer: params.Manufacturer,
+        ManufacturerCode: params.ManufacturerCode,
+        Type: params.Type,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/AddAnalyzerSysMN', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 获取分析仪实体
 export async function getanalyzersysmnmodel(params) {
-  const body = {
-    ID: params.ID,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerSysMNModel', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID: params.ID,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerSysMNModel', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 编辑分析仪实体
 export async function editalyzersys(params) {
-  const body = {
-    ID: params.ID,
-    Manufacturer: params.Manufacturer,
-    ManufacturerCode: params.ManufacturerCode,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/EditAnalyzerSysMN', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID: params.ID,
+        Manufacturer: params.Manufacturer,
+        ManufacturerCode: params.ManufacturerCode,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/EditAnalyzerSysMN', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 删除分析仪
 export async function deletealyzersys(params) {
-  const body = {
-    ID: params.ID,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/DeleteAnalyzerSysMN', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID: params.ID,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/DeleteAnalyzerSysMN', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 获取测试项目
 export async function getcomponent(params) {
-  const body = {
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/GetComponent', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/GetComponent', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 添加设备分析仪
 export async function addalyzerchild(params) {
-  const body = {
+    const body = {
         DGIMN: params.DGIMN,
         Name: params.Name,
         DeviceModel: params.DeviceModel,
@@ -175,60 +177,80 @@ export async function addalyzerchild(params) {
         Intercept: params.Intercept,
         AnalyzerSys_Id: params.AnalyzerSys_Id,
         AnalyzerRangeMax: params.AnalyzerRangeMax,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/AddAlyzerChild', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/AddAlyzerChild', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 获取设备下的子集
 export async function getanalyzerchild(params) {
-  const body = {
-      ID:params.ID,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzer', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID:params.ID,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzer', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 获取分析仪实体
 export async function getanalyzerchildmodel(params) {
-  const body = {
-    ID: params.ID,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerChildModel', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID: params.ID,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/GetAnalyzerChildModel', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 删除分析仪
 export async function deletealyzerchild(params) {
-  const body = {
-    ID: params.ID,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/DeleteAnalyzerChild', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID: params.ID,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/DeleteAnalyzerChild', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
 // 编辑分析仪
 export async function editalyzerchild(params) {
-  const body = {
-    ID:params.ID,
-    Name: params.Name,
-    DeviceModel: params.DeviceModel,
-    Manufacturer: params.Manufacturer,
-    ManufacturerAbbreviation: params.ManufacturerAbbreviation,
-    TestComponent: params.TestComponent,
-    AnalyzerPrinciple: params.AnalyzerPrinciple,
-    AnalyzerRangeMin: params.AnalyzerRangeMin,
-    MeasurementUnit: params.MeasurementUnit,
-    Slope: params.Slope,
-    Intercept: params.Intercept,
-    AnalyzerRangeMax: params.AnalyzerRangeMax,
-  };
-  const result = post('/api/rest/PollutantSourceApi/Analyzer/EditAlyzerChild', body, null);
-  return result === null ? {
-    data: null
-  } : result;
+    const body = {
+        ID:params.ID,
+        Name: params.Name,
+        DeviceModel: params.DeviceModel,
+        Manufacturer: params.Manufacturer,
+        ManufacturerAbbreviation: params.ManufacturerAbbreviation,
+        TestComponent: params.TestComponent,
+        AnalyzerPrinciple: params.AnalyzerPrinciple,
+        AnalyzerRangeMin: params.AnalyzerRangeMin,
+        MeasurementUnit: params.MeasurementUnit,
+        Slope: params.Slope,
+        Intercept: params.Intercept,
+        AnalyzerRangeMax: params.AnalyzerRangeMax,
+    };
+    const result = post('/api/rest/PollutantSourceApi/Analyzer/EditAlyzerChild', body, null);
+    return result === null ? {
+        data: null
+    } : result;
+}
+// 获取污染物类型
+export async function getpollutanttypelist(params) {
+    const body = {
+    };
+    const result = post('/api/rest/PollutantSourceApi/ManualSupplement/GetPollutantTypeList', body, null);
+    return result === null ? {
+        data: null
+    } : result;
+}
+// 获取气排口类型
+export async function getgasoutputtypelist(params) {
+    const body = {
+        name: '',
+        code: '',
+    };
+    const result = post('/api/rest/PollutantSourceApi/PPointAndData/GasOutputTypeList', body, null);
+    return result === null ? {
+        data: null
+    } : result;
 }
