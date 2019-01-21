80847b989dd90ac30f12660e1429d0283ee56162
wjwgit <w417287508@163.com>
2019-01-21 18:16:32 +0800

    update

--- src/pages/SpecialWorkbench/index.js
+++ src/pages/SpecialWorkbench/index.js
@@ -50,110 +50,6 @@ class SpecialWorkbench extends Component {
 
     }
 
-<<<<<<< HEAD
-    /**
-     * 智能质控_渲染异常报警数据列表
-     */
-    renderExceptionAlarmList = () => {
-        let listData = [];
-        const { exceptionAlarm } = this.props;
-        const colorArray = {
-            "数据超标": "red",
-            "超标预警": "blue",
-            "数据异常": "gold",
-            "参数异常": "yellow",
-            "逻辑异常": "volcano",
-            "状态异常": "orange"
-        };
-        listData = exceptionAlarm.tableDatas.map((item) => {
-            //判断报警是否超过4小时
-            const seconds = moment().diff(moment(item.FirstAlarmTime), 'minutes');
-            const hour = Math.floor(seconds / 60);
-            const minutes = Math.floor(seconds % 60);
-            const color = hour >= 4 ? 'red' : 'rgb(129,203,237)';
-            const minutesLable = minutes > 0 ? `${minutes}分钟` : '';
-
-            const labelDiv = <div style={{ color: `${color}` }}>已发生{hour}小时{minutesLable}</div>;
-            //未响应，按钮是派单;响应了超过4个小时是督办
-            let btnDiv = '';
-            // if (item.State === "0") {
-            //     btnDiv = hour >= 4 ? (
-            //         <div style={{ marginTop: 43 }}>
-            //             <Button
-            //                 onClick={() => {
-            //                     this.urge(item.DGIMNs);
-            //                 }}
-            //                 style={{ width: 100, border: 'none', backgroundColor: 'rgb(74,210,187)' }}
-            //                 type="primary"
-            //             >督办
-            //             </Button>
-            //         </div>
-            //     ) : '';
-            // } else if (item.State === "1") {
-            //     btnDiv = (
-            //         <div style={{ marginTop: 43 }}>
-            //             <Button
-            //                 onClick={() => {
-            //                     this.setState({
-            //                         pdvisible: true,
-            //                         selectpoint: null
-            //                     });
-            //                 }}
-            //                 style={{ width: 100, border: 'none', backgroundColor: 'rgb(74,210,187)' }}
-            //                 type="primary"
-            //             >派单
-            //             </Button>
-            //         </div>
-            //     );
-            // }
-            if (item.State==="0") {
-                btnDiv=(
-                    <div style={{marginTop:43}}>
-                        <Button
-                            onClick={() => {
-                                this.setState({
-                                    pdvisible: true,
-                                    selectpoint: null
-                                });
-                                
-                            }}
-                            style={{width:100,border:'none',backgroundColor:'rgb(74,210,187)'}}
-                            type="primary"
-                        >派单
-                        </Button>
-                    </div>
-                );
-            }else if(item.State==="1"){
-                btnDiv=hour>= 4 ?(
-                    <div style={{marginTop:43}}>
-                        <Button
-                            onClick={()=>{
-                                this.urge(item.DGIMNs);
-                            }}
-                            style={{width:100,border:'none',backgroundColor:'rgb(74,210,187)'}}
-                            type="primary"
-                        >督办
-                        </Button>
-                    </div>
-                ):'';
-            }
-            return {
-                href: 'http://ant.design',
-                title: `${item.PointName}`,
-                avatar: (<Icon type="alert" theme="twoTone" />),
-                description: (
-                    <div>
-                        <div>
-                            {
-                                item.ExceptionTypes.split(',').map(item => (
-                                    <Tag color={`${colorArray[item]}`}>{item}</Tag>
-                                ))
-                            }
-                        </div>
-                        <div style={{ marginTop: 10 }}>
-                            <div>{item.LastAlarmMsg}</div>
-                        </div>
-=======
     render() {
         return (
             <div
@@ -191,7 +87,6 @@ class SpecialWorkbench extends Component {
 
                     <div className={styles.headerDiv}>
                         <p>智能运维</p>
->>>>>>> 00e7dd123ba2c1ee03b1795b658d0fc7cef0309c
                     </div>
                     <OperationCalendar />
                 </div>
