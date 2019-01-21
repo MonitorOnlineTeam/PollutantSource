ddd90e8192eca904801d546ea1e0ec5019399272
dongxiaoyun123 <1227906469@qq.com>
2019-01-21 18:08:30 +0800

    修改

---
+++ src/components/SpecialWorkbench/IntelligentQualityControl.js
@@ -0,0 +1,307 @@
+import React, { Component } from 'react';
+import { Row, Col, Card, Table, Button, Tabs } from 'antd';
+import ReactEcharts from 'echarts-for-react';
+import moment from 'moment';
+import { connect } from 'dva';
+
+const gridStyle = {
+    width: '50%',
+    textAlign: 'center',
+    height: '200px'
+};
+
+@connect(({
+    loading,
+    workbenchmodel,
+    transmissionefficiency,
+    equipmentoperatingrate,
+}) => ({
+    loadingRateStatistics: loading.effects['workbenchmodel/getRateStatisticsData'],
+    loadingNetworkeRate: loading.effects['workbenchmodel/getNetworkeRateData'],
+    loadingEquipmentoperatingRate: loading.effects['equipmentoperatingrate/getData'],
+    loadingTransmissionefficiencyRate: loading.effects['transmissionefficiency/getData'],
+    rateStatistics: workbenchmodel.rateStatistics,
+    networkeRateList: workbenchmodel.networkeRateList,
+    equipmentoperatingRateTableDatas: equipmentoperatingrate.tableDatas,
+    transmissionefficiencyRateTableDatas: transmissionefficiency.tableDatas,
+}))
+
+/**
+ * 智能质控_加载图表数据(3)
+ */
+
+class IntelligentQualityControl extends Component {
+    componentWillMount() {
+        this.getRateStatisticsData();
+        this.getNetworkeRateData();
+        this.getEquipmentoperatingRateData();
+        this.getTransmissionefficiencyRateData();
+    }
+
+    /**
+     * 智能质控_率的统计_更新数据
+     */
+    getRateStatisticsData = () => {
+        this.props.dispatch({
+            type: 'workbenchmodel/getRateStatisticsData',
+            payload: {},
+        });
+    }
+
+    /**
+     * 智能质控_排口联网率_更新数据
+     */
+    getNetworkeRateData = () => {
+        this.props.dispatch({
+            type: 'workbenchmodel/getNetworkeRateData',
+            payload: {},
+        });
+    }
+
+    /**
+     * 智能质控_排口设备运转率_更新数据
+     */
+    getEquipmentoperatingRateData = (pageIndex) => {
+        this.props.dispatch({
+            type: 'equipmentoperatingrate/getData',
+            payload: {
+                pageIndex: pageIndex || 1,
+            }
+        });
+    }
+
+    /**
+     * 智能质控_排口传输有效率_更新数据
+     */
+    getTransmissionefficiencyRateData = (pageIndex) => {
+        this.props.dispatch({
+            type: 'transmissionefficiency/getData',
+            payload: {
+                pageIndex: pageIndex || 1,
+            }
+        });
+    }
+
+    getOption = (type) => {
+        const { model } = this.props.rateStatistics;
+        let networkeRate = (parseFloat(model.NetworkeRate) * 100).toFixed(2);
+        let runningRate = (parseFloat(model.RunningRate) * 100).toFixed(2);
+        let transmissionEffectiveRate = (parseFloat(model.TransmissionEffectiveRate) * 100).toFixed(2);
+        let legendData = [];
+        let color = [];
+        let seriesName = '';
+        let seriesData = [];
+        if (type === 1) {
+            legendData = ['正常', '离线'];
+            color = ['rgb(48,155,86)', 'rgb(245,68,66)'];
+            seriesName = '实时联网率';
+            seriesData = [
+                { value: networkeRate, name: '正常' },
+                { value: (100 - networkeRate).toFixed(2), name: '离线' }
+            ];
+        } else if (type === 2) {
+            legendData = ['达标', '未达标'];
+            color = ['rgb(48,155,86)', 'rgb(245,68,66)'];
+            seriesName = '设备运转率';
+            seriesData = [
+                { value: runningRate, name: '达标' },
+                { value: (100 - runningRate).toFixed(2), name: '未达标' }
+            ];
+        } else {
+            legendData = ['达标', '未达标'];
+            color = ['rgb(48,155,86)', 'rgb(245,68,66)'];
+            seriesName = '传输有效率';
+            seriesData = [
+                { value: transmissionEffectiveRate, name: '达标' },
+                { value: (100 - transmissionEffectiveRate).toFixed(2), name: '未达标' }
+            ];
+        }
+        let option = {
+            color: color,
+            tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
+            },
+            legend: {
+                orient: 'vertical',
+                x: 'left',
+                data: legendData
+            },
+            series: [
+                {
+                    name: seriesName,
+                    type: 'pie',
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+
+                    itemStyle: {
+                        normal: {
+                            label: {
+                                // formatter : function (params){
+                                //     return params.value + '% <br/>详情情况';
+                                // },
+                                formatter: "{b}: {c}%",
+                                textStyle: {
+                                    baseline: 'top'
+                                }
+                            }
+                        },
+                    },
+                    data: seriesData
+                }
+            ]
+        };
+        return option;
+    }
+
+    /**
+     * 智能质控_渲染排口联网率表格
+     */
+    renderNetWorkingRateTable = () => {
+        const columns = [
+            {
+                title: '排口名称',
+                dataIndex: 'PointName'
+            },
+            {
+                title: '联网状态',
+                dataIndex: 'RateValue',
+                render: (text, record) => {
+                    if (text === 100)
+                        return `${(parseFloat(text) * 100).toFixed(2)}%`;
+                    return <span style={{ color: 'red' }}>离线</span>;
+                }
+            }];
+
+        return <Table key="network" loading={this.props.loadingNetworkeRate} columns={columns} dataSource={this.props.networkeRateList.tableDatas.slice(0, 3)} size="small" pagination={false} />;
+    }
+
+    /**
+     * 智能质控_渲染排口设备运转率表格
+     */
+    renderEquipmentoperatingRateTable = () => {
+        const columns = [
+            {
+                title: '排口名称',
+                dataIndex: 'PointName'
+            },
+            {
+                title: '设备运转率',
+                dataIndex: 'RunningRate',
+                render: (text, record) => {
+                    let rr = `${(parseFloat(text) * 100).toFixed(2)}%`;
+                    if (text >= 90)
+                        return rr;
+                    return <span style={{ color: 'red' }}>{rr}</span>;
+                }
+            }];
+        return <Table key="runrate" loading={this.props.loadingEquipmentoperatingRate} columns={columns} dataSource={this.props.equipmentoperatingRateTableDatas.slice(0, 3)} size="small" pagination={false} />;
+    }
+
+    /**
+     * 智能质控_渲染排口传输有效率表格
+     */
+    renderTransmissionefficiencyRateTable = () => {
+        const columns = [
+            {
+                title: '排口名称',
+                dataIndex: 'PointName'
+            },
+            {
+                title: '传输有效率',
+                dataIndex: 'TransmissionEffectiveRate',
+                render: (text, record) => {
+                    let rr = `${(parseFloat(text) * 100).toFixed(2)}%`;
+                    if (text >= 90)
+                        return rr;
+                    return <span style={{ color: 'red' }}>{rr}</span>;
+                }
+            }];
+
+        return <Table key="effectrate" loading={this.props.loadingTransmissionefficiencyRate} columns={columns} dataSource={this.props.transmissionefficiencyRateTableDatas.slice(0, 3)} size="small" pagination={false} />;
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col span={24}>
+                        <Card title="实时联网率" style={{}} extra={<a href="/overview/datalistview">更多&gt;&gt;</a>}>
+                            <Card.Grid style={gridStyle}>
+                                <ReactEcharts
+                                    loadingOption={this.props.loadingRateStatistics}
+                                    option={this.getOption(1)}
+                                    style={{ height: '150px', width: '100%' }}
+                                    className="echarts-for-echarts"
+                                    onEvents={{ 'click': this.onChartClick }}
+                                    theme="my_theme"
+                                />
+                            </Card.Grid>
+                            <Card.Grid style={gridStyle}>
+                                {
+                                    this.renderNetWorkingRateTable()
+                                }
+                            </Card.Grid>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Card
+                            title="当月设备运转率"
+                            style={{ marginTop: 10 }}
+                            extra={<a href="/qualitycontrol/equipmentoperatingrate">更多&gt;&gt;</a>}
+                        >
+                            <Card.Grid style={gridStyle}>
+                                <ReactEcharts
+                                    loadingOption={this.props.loadingRateStatistics}
+                                    option={this.getOption(2)}
+                                    style={{ height: '150px', width: '100%' }}
+                                    className="echarts-for-echarts"
+                                    onEvents={{ 'click': this.onChartClick }}
+                                    theme="my_theme"
+                                />
+
+                            </Card.Grid>
+                            <Card.Grid style={gridStyle}>
+                                {
+                                    this.renderEquipmentoperatingRateTable()
+                                }
+                            </Card.Grid>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Card
+                            title="当月传输有效率"
+                            style={{ marginTop: 10 }}
+                            extra={<a
+                                href="/qualitycontrol/transmissionefficiency"
+                            >更多>>
+                            </a>}
+                        >
+                            <Card.Grid style={gridStyle} loading={this.props.loadingRateStatistics}>
+                                <ReactEcharts
+                                    option={this.getOption(3)}
+                                    style={{ height: '150px', width: '100%' }}
+                                    className="echarts-for-echarts"
+                                    onEvents={{ 'click': this.onChartClick }}
+                                    theme="my_theme"
+                                />
+
+                            </Card.Grid>
+                            <Card.Grid style={gridStyle}>
+                                {
+                                    this.renderTransmissionefficiencyRateTable()
+                                }
+                            </Card.Grid>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default IntelligentQualityControl;
