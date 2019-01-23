e2755fe0ebdb90826f6d092998a2bfa0ca5b77e2
dongxiaoyun123 <1227906469@qq.com>
2019-01-23 10:58:31 +0800

    修改

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -150,11 +150,7 @@ class HeaderView extends PureComponent {
   }
 }
 
-export default connect(({ user, global, setting, loading }) => ({
-    currentUser: user.currentUser,
+export default connect(({global, setting}) => ({
     collapsed: global.collapsed,
-    fetchingNotices: loading.effects['global/fetchNotices'],
-    notices: global.notices,
-    currentUserNoticeCnt: global.currentUserNoticeCnt,
     setting,
 }))(HeaderView);
