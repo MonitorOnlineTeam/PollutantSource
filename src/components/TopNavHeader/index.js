746d62593e7fe1d465cbcd939b259dcb1e271dc6
unknown <15811372184@163.com>
2019-03-29 15:09:05 +0800

    修改bug

--- src/components/TopNavHeader/index.js
+++ src/components/TopNavHeader/index.js
@@ -29,10 +29,10 @@ export default class TopNavHeader extends PureComponent {
               >
                   <div className={styles.left}>
                       <div className={styles.logo} key="logo" id="logo">
-                          <Link to="/homepage">
+                          {/* <Link to="/homepage"> */}
                               <img src={logo} alt="logo" />
                               <h1>污染源智能分析平台</h1>
-                          </Link>
+                          {/* </Link> */}
                       </div>
                       <div
                           style={{
